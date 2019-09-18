f0b2a7e60bf2ee1ee27f4552f3ee264da0ecf769
Ashraf Waqar <waqarashraf22@gmail.com>
2019-09-18 17:27:47 +0300

    Fetches data from API and set state in Home compenent

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from '../../config';
+
 import MovieImage from '../layout/MovieImage/MovieImage';
 import SearchBar from '../layout/SearchBar/SearchBar';
 import Grid from '../layout/Grid/Grid';
@@ -9,7 +11,45 @@ import './Home.css';
 
 class Home extends Component {
   state = {
+    movies: [],
+    movieImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ''
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
 
+  loadMoreItems = () => {
+    let endpoint = '';
+    this.setState({ loading: true });
+
+    if (this.state.searchTerm === '') {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = (endpoint) => {
+    fetch(endpoint)
+    .then(result => result.json())
+    .then(result => {
+      this.setState({
+        //copying the old movies and appending the new movies 
+        movies: [...this.state.movies, ...result.results],
+        movieImage: this.state.movieImage || result.results[0],
+        loading: false,
+        currentPage: result.page,
+        totalPages: result.total_pages
+      })
+    })
+    .catch(error => console.error('Error:', error))
   }
 
   render() {
